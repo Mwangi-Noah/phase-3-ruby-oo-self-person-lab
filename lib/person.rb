8f78100a95c75beb10b161311051f8e6fccc7cf4
Noah <noahmwangi@gmail.com>
2023-05-25 07:14:24 +0300

    Completed Assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,105 @@
-# your code goes here
+class Person
+  attr_reader :name
+  attr_accessor :bank_account, :happiness, :hygiene
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def bank_account=(value)
+    @bank_account = value
+  end
+  
+  def happiness=(value)
+    @happiness = extent_limit(value,0, 10)
+  end
+
+  def hygiene=(value)
+    @happiness = extent_limit(value,0, 10)
+  end
+
+  def clean?
+    hygiene > 7
+  end
+
+  def happy?
+    happiness > 7
+  end
+
+  def get_paid(amount)
+    self.bank_account += amount
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end 
+
+  def clean?
+    hygiene > 7
+  end
+
+  def happy?
+    happiness > 7
+  end
+
+  def get_paid(amount)
+    self.bank_account += amount
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+  def work_out
+    self.happiness += 2
+    self.hygiene -= 3
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(friend, topic)
+    case topic
+    when "politics"
+      self.happiness -= 2
+      friend.happiness -= 2
+      "blah blah partisan blah lobbyist"
+    when "weather"
+      self.happiness += 1
+      friend.happiness += 1
+      "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
+    end
+  end
+
+  private
+  def extent_limit(value, min, max)
+    if value < min
+      min
+    elsif value > max
+      max
+    else
+      value
+    end
+  end
+end
+
+person = Person.new("Noah")
+puts person.clean?
+puts person.happy?
+puts person.get_paid(1000)
+puts person.take_bath
+puts person.work_out  
+friend = Person.new("Mwangi")
+
